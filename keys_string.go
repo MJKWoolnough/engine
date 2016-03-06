f798917dd6d4d61df11f431fff3a8339d419011e
Michael Woolnough <michael.woolnough@gmail.com>
2016-03-06 14:04:53 +0000

    Added keys stringifier

---
+++ keys_string.go
@@ -0,0 +1,13 @@
+package engine
+
+var (
+	keyStrings = "EscapeUpDownLeftRightLeftMiddleRightMouse Scroll UpMouse Scroll Down"
+	keyIndexes = []uint16{0, 6, 8, 12, 16, 21, 25, 31, 36, 51, 68}
+)
+
+func (k Key) String() string {
+	if k >= 10 {
+		return "UNKNOWN"
+	}
+	return keyStrings[keyIndexes[k]:keyIndexes[k+1]]
+}
---
+++ keys_string.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+(
+
+echo "package engine";
+echo;
+echo "var (";
+echo -n "	keyStrings = "\";
+	mode=false;
+	declare -a indexes;
+	num=0;
+	total=0;
+	while read line; do
+		if $mode; then
+			if [ "$line" = ")" ]; then
+				mode=false;
+			else
+				name="$(echo "$line" | cut -d'/' -f3 | tr -d "\n")";
+				let "total += ${#name}";
+				indexes[$num]=$total;
+				echo -n "$name";
+				let "num++";
+			fi;
+		else
+			if [ "$line" = "const (" ]; then
+				mode=true;
+			fi;
+		fi;
+	done < keys.go;
+
+	echo "\"";
+	echo -n "	keyIndexes = []uint16{0";
+	for index in ${indexes[@]}; do
+		echo -n ", $index";
+	done;
+	echo "}";
+	echo ")";
+	echo;
+	echo "func (k Key) String() string {";
+	echo "	if k >= $num {";
+	echo "		return \"UNKNOWN\"";
+	echo "	}";
+	echo "	return keyStrings[keyIndexes[k]:keyIndexes[k+1]]";
+	echo "}";
+) > keys_string.go
