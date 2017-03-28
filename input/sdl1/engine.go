f67b8e063a4f979f6cad3a6a4700cfd33b1a500a
Michael Woolnough <michael.woolnough@gmail.com>
2017-03-28 14:56:49 +0100

    added sdl1 engines

---
+++ input/sdl1/engine.go
@@ -0,0 +1,73 @@
+package sdl1
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+
+	"github.com/MJKWoolnough/engine"
+	"github.com/banthar/Go-SDL/sdl"
+)
+
+type sdlengine struct {
+	keys [256]bool
+}
+
+const eventSubsystem = 0x4000
+
+func init() {
+	runtime.LockOSThread()
+	if err := sdl.Init(eventSubsystem); err != 0 {
+		panic(errors.New(sdl.GetError()))
+	}
+	engine.RegisterInput(new(sdlengine))
+}
+
+func (s *sdlengine) InputInit() error {
+	return nil
+}
+
+func (s *sdlengine) InputUninit() error {
+	sdl.QuitSubSystem(eventSubsystem)
+	return nil
+}
+
+func (s *sdlengine) Poll() {
+	for e := sdl.PollEvent(); e != nil; e = sdl.PollEvent() {
+		fmt.Printf("%#v\n", e)
+		switch e := e.(type) {
+		case *sdl.KeyboardEvent:
+			s.setKey(e.Keysym, e.State == 1)
+		case *sdl.MouseButtonEvent:
+			s.setMouse(e.Button, e.State == sdl.MOUSEBUTTONDOWN)
+			//case sdl.MouseMotionEvent:
+		}
+	}
+}
+
+func (s *sdlengine) KeyPressed(k engine.Key) bool {
+	return s.keys[k]
+}
+
+func (s *sdlengine) CursorPos() (float64, float64) {
+	var x, y int
+	sdl.GetMouseState(&x, &y)
+	return float64(x), float64(y)
+}
+
+func (s *sdlengine) setKey(k sdl.Keysym, down bool) {
+	switch k.Sym {
+	case sdl.K_ESCAPE:
+		s.keys[engine.KeyEscape] = down
+	}
+}
+
+func (s *sdlengine) setMouse(b uint8, down bool) {
+	switch b {
+	case sdl.BUTTON_LEFT:
+	case sdl.BUTTON_RIGHT:
+	case sdl.BUTTON_MIDDLE:
+	case sdl.BUTTON_X1:
+	case sdl.BUTTON_X2:
+	}
+}
---
+++ windows/sdl1/engine.go
@@ -0,0 +1,110 @@
+package sdl1
+
+import (
+	"runtime"
+	"sort"
+	"sync/atomic"
+
+	"github.com/MJKWoolnough/engine"
+	_ "github.com/MJKWoolnough/engine/input/sdl1"
+	"github.com/banthar/Go-SDL/sdl"
+)
+
+type sdlengine struct {
+	surface *sdl.Surface
+	quit    uint32
+	gl      bool
+}
+
+func init() {
+	runtime.LockOSThread()
+	if err := sdl.Init(sdl.INIT_VIDEO | sdl.INIT_TIMER); err != 0 {
+		panic(sdl.GetError())
+	}
+	s := new(sdlengine)
+	engine.RegisterWindow(s)
+}
+
+func (s *sdlengine) WindowInit(c engine.Config) error {
+	graphicsID := engine.GLID()
+	var (
+		flags uint32
+	)
+	if len(graphicsID) >= 2 && graphicsID[0] == 'G' && graphicsID[1] == 'L' {
+		sdl.GL_SetAttribute(sdl.GL_DOUBLEBUFFER, 1)
+		sdl.GL_SetAttribute(sdl.GL_RED_SIZE, 8)
+		sdl.GL_SetAttribute(sdl.GL_BLUE_SIZE, 8)
+		sdl.GL_SetAttribute(sdl.GL_GREEN_SIZE, 8)
+		sdl.GL_SetAttribute(sdl.GL_ALPHA_SIZE, 8)
+		flags |= sdl.OPENGL
+		s.gl = true
+	}
+	if c.Monitor != nil {
+		flags |= sdl.FULLSCREEN | sdl.HWACCEL
+	}
+
+	s.surface = sdl.SetVideoMode(c.Mode.Width, c.Mode.Height, 32, flags)
+
+	return nil
+}
+
+func (s *sdlengine) Loop(run func(int, int, float64)) {
+	for atomic.LoadUint32(&s.quit) == 0 {
+		engine.PollInput()
+		vi := sdl.GetVideoInfo()
+		t := float64(sdl.GetTicks()) / 1000
+		run(int(vi.Current_w), int(vi.Current_h), t)
+		if s.gl {
+			sdl.GL_SwapBuffers()
+		}
+	}
+}
+
+func (s *sdlengine) Close() {
+	atomic.StoreUint32(&s.quit, 1)
+}
+
+func (s *sdlengine) WindowUninit() error {
+	s.surface.Free()
+	sdl.Quit()
+	return nil
+}
+
+func (s *sdlengine) GetMonitors() []*engine.Monitor {
+	return []*engine.Monitor{engine.NewMonitor("DEFAULT", 0)}
+}
+
+type modeList []engine.Mode
+
+func (m modeList) Len() int {
+	return len(m)
+}
+
+func (m modeList) Less(i, j int) bool {
+	if m[i].Width < m[j].Width {
+		return true
+	} else if m[j].Width < m[i].Width {
+		return false
+	}
+	return m[i].Height < m[j].Height
+}
+
+func (m modeList) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func (s *sdlengine) GetModes(m interface{}) []engine.Mode {
+	ms := sdl.ListModes(nil, sdl.FULLSCREEN|sdl.HWACCEL)
+	modes := make([]engine.Mode, len(ms))
+	for n, mode := range ms {
+		modes[n].Width = int(mode.W)
+		modes[n].Height = int(mode.H)
+		modes[n].Refresh = 60
+	}
+	sort.Sort(modeList(modes))
+	return modes
+}
+
+func (s *sdlengine) SetMode(m interface{}, mode engine.Mode) {
+	//s.window.SetDisplayMode
+}
