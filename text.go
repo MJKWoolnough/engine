6d5c0eb4487b81005e247ef485ac7a711c7b889e
Michael Woolnough <michael.woolnough@gmail.com>
2017-05-14 16:02:30 +0100

    First run at TTF decoding

--- text.go
+++ text.go
@@ -11,11 +11,13 @@ import (
 )
 
 type TTF struct {
-	Advances []fixed.Int26_6
-	Boxes    []fixed.Rectangle26_6
+	Advances []float32
+	Boxes    [][4]float32
 	Pos      [][2]int
 	Coords   []float32
 }
+
+const em = 8192 // 128 << 6 // for fixed.Int26_6
 
 func DecodeTTF(r io.Reader, start, end rune) (*TTF, error) {
 	data, err := ioutil.ReadAll(r)
@@ -27,26 +29,102 @@ func DecodeTTF(r io.Reader, start, end rune) (*TTF, error) {
 		return nil, err
 	}
 
-	var g truetype.GlyphBuf
-	coords := make([]float32, 0, 1024)
+	var (
+		g truetype.GlyphBuf
+		v VertexBuilder
+	)
 	pos := make([][2]int, 0, end-start+1)
 	advances := make([]fixed.Int26_6, 0, end-start+1)
 	boxes := make([]fixed.Rectangle26_6, 0, end-start+1)
 
 	for r := start; r <= end; r++ {
-		err = g.Load(f, fixed.I(100), f.Index(r), font.HintingNone)
+		err = g.Load(f, em, f.Index(r), font.HintingNone)
 		if err != nil {
 			return nil, err
 		}
-		advances = append(advances, g.AdvanceWidth)
-		boxes = append(boxes, g.Bounds)
-
+		advances = append(advances, float32(g.AdvanceWidth)/em)
+		boxes = append(boxes, [4]float32{
+			float32(g.Bounds.Min.X) / em,
+			float32(g.Bounds.Min.Y) / em,
+			float32(g.Bounds.Max.X) / em,
+			float32(g.Bounds.Max.Y) / em,
+		})
+		firstPos := len(v.Vertices)
+		prevPos := 0
+		for _, e := range g.Ends {
+			lastPoint := g.Points[prevPos]
+			v.MoveTo(lastPoint.X, lastPoint.Y)
+			for _, point := range g.Points[prevPos+1 : e] {
+				if point.Flags&1 == 0 && lastPoint.Flags&1 == 0 {
+					v.CurveTo(lastPoint.X, lastPoint.Y, (point.X+lastPoint.X)/2, (point.Y+lastPoint.Y)/2)
+					lastPoint.X = (point.X + lastPoint.X) / 2
+					lastPoint.Y = (point.Y + lastPoint.Y) / 2
+				}
+				if point.Flags&1 == 1 {
+					if lastPoint.Flags&1 == 1 {
+						v.LineTo(point.X, point.Y)
+					} else {
+						v.CurveTo(lastPoint.X, lastPoint.Y, point.X, point.Y)
+					}
+				}
+				lastPoint = point
+			}
+			prevPos += e
+		}
+		pos = append(pos, [2]int{first, len(v.Vertices) - firstPos})
 	}
 
 	return &TTF{
 		Advances: advances,
 		Boxes:    boxes,
 		Pos:      pos,
-		Coords:   coords,
+		Coords:   v.Vertices,
 	}, nil
+}
+
+type VertexBuilder struct {
+	start, current [2]fixed.Point26_6
+	count          int
+	Vertices       []float32
+}
+
+func (v *VertexBuilder) MoveTo(point fixed.Point26_6) {
+	v.start = [2]fixed.Point26_6{x, y}
+	v.current = g.start
+	v.count = 0
+}
+
+func (v *VertexBuilder) LineTo(x, y fixed.Int26_6) {
+	v.count++
+	if v.count >= 2 {
+		v.addTriangle(v.start[0], v.start[1], v.current[0], v.current[1], x, y)
+	}
+	v.current[0] = x
+	v.current[1] = y
+}
+
+func (v *VertexBuilder) CurveTo(cx, cy, x, y fixed.Int26_6) {
+	g.count++
+	if v.count >= 2 {
+		v.addTriangle(v.start[0], v.start[1], v.current[0], v.current[1], x, y)
+	}
+	v.addCurve(v.current[0], v.current[1], cx, cy, x, y)
+	v.current[0] = x
+	v.current[1] = y
+}
+
+func (v *VertexBuilder) addTriangle(ax, ay, bx, by, cx, cy fixed.Point26_6) {
+	v.addVertex(ax, ay, 0, 1)
+	v.addVertex(bx, by, 0, 1)
+	v.addVertex(cx, cy, 0, 1)
+}
+
+func (v *VertexBuilder) addCurve(ax, ay, bx, by, cx, cy fixed.Point26_6) {
+	v.addVertex(ax, ay, 0, 0)
+	v.addVertex(bx, by, 0.5, 0)
+	v.addVertex(cx, cy, 1, 1)
+}
+
+func (v *VertexBuilder) addVertex(x, y fixed.Point26_6, s, t float32) {
+	v.vertices = append(v.vertices, float32(x)/em, float32(y)/em, s, t)
 }
